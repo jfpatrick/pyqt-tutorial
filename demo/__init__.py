8490fe7bbc4c02af438f425859b2dc7dbddfeb85
Sara Zanzottera <sara.zanzottera@cern.ch>
2020-06-04 17:54:02 +0200

    Add pyqt5ac line into demo/__init__.py

---
+++ demo/__init__.py
@@ -0,0 +1,5 @@
+
+# Generate the code from .ui and .qrc files in case they are missing or outdated
+import os
+import pyqt5ac
+pyqt5ac.main(config=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pyqt5ac.yml'))
